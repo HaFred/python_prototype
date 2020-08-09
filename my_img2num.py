c75b4f1de5bd95186d38be61fb317ee13575507f
HaFred <zhongad@connect.ust.hk>
2020-08-09 15:11:00 +0800

    Made the GD inside the BP process, but this one tried for out-of-order GD

--- my_img2num.py
+++ my_img2num.py
@@ -8,137 +8,135 @@ import torch.utils.data.dataloader as dataloader
 import time
 import numpy as np
 
+
 class MyImg2Num:
-  def __init__(self):
-    self.train_batch_size=64
-    self.test_batch_size=1000
-    self.input_size=784
-    self.output_size=10
-    self.model=NeuralNetwork(784,500,100,10)
-    self.image_h=28
-    self.image_w=28
-    self.image_d=1
-    self.pixels=self.image_h*self.image_w*self.image_d
-    self.learning_rate=0.1
-    self.num_epochs=2
+    def __init__(self):
+        self.train_batch_size = 64
+        self.test_batch_size = 1000
+        self.input_size = 784
+        self.output_size = 10
+        self.model = NeuralNetwork(784, 500, 100, 10)
+        self.image_h = 28
+        self.image_w = 28
+        self.image_d = 1
+        self.pixels = self.image_h * self.image_w * self.image_d
+        self.learning_rate = 0.1
+        self.num_epochs = 3
 
+    def forward(self, img_batch):
+        input_vector = img_batch.view(-1, self.pixels)
+        output = self.model.forward(torch.t(input_vector))
+        return torch.t(output)
 
-  def forward(self,img_batch):
-    input_vector=img_batch.view(-1,self.pixels)
-    output=self.model.forward(torch.t(input_vector))
-    return torch.t(output)
+    def train(self):
+        def one_hot_encoding(input_num):
+            one_hot = torch.zeros(10, dtype=torch.short)
+            one_hot[input_num] = 1
+            return one_hot
 
-  def train(self):
-    def one_hot_encoding(input_num):
-      one_hot=torch.zeros(10,dtype=torch.short)
-      one_hot[input_num]=1
-      return one_hot
+        loss_train = []
+        loss_test = []
+        correct_batch = 0
+        correct_batch_test = 0
+        correct_epoch = []
+        correct_epoch_test = []
 
-    loss_train=[]
-    loss_test=[]
-    correct_batch=0
-    correct_batch_test=0
-    correct_epoch=[]
-    correct_epoch_test=[]
+        ###LOAD DATA
+        train = MNIST('./data', train=True, download=True, transform=transforms.Compose([
+            transforms.ToTensor(),
+            # transforms.Normalize((0.1307,), (0.3081,)),# ToTensor does min-max normalization.
+        ]), )
 
-###LOAD DATA
-    train = MNIST('./data', train=True, download=True, transform=transforms.Compose([
-        transforms.ToTensor(),  
-        #transforms.Normalize((0.1307,), (0.3081,)),# ToTensor does min-max normalization. 
-    ]), )
-    
-    test = MNIST('./data', train=False, download=True, transform=transforms.Compose([
-        transforms.ToTensor(),  
-        #transforms.Normalize((0.1307,), (0.3081,)),# ToTensor does min-max normalization. 
-    ]), )
-    
-    # Create DataLoader
-    dataloader_args = dict(shuffle=True, batch_size=64) #data loader reshuffles every step
-    train_loader = dataloader.DataLoader(train, **dataloader_args)
-    test_loader = dataloader.DataLoader(test, **dataloader_args)
-    
-    train_data = train.train_data
-    train_data = train.transform(train_data.numpy())
+        test = MNIST('./data', train=False, download=True, transform=transforms.Compose([
+            transforms.ToTensor(),
+            # transforms.Normalize((0.1307,), (0.3081,)),# ToTensor does min-max normalization.
+        ]), )
 
+        # Create DataLoader
+        dataloader_args = dict(shuffle=True, batch_size=64)  # data loader reshuffles every step
+        train_loader = dataloader.DataLoader(train, **dataloader_args)
+        test_loader = dataloader.DataLoader(test, **dataloader_args)
 
+        train_data = train.train_data
+        train_data = train.transform(train_data.numpy())
 
-###TRAINING
-    time_meas=torch.Tensor(self.num_epochs)
-    acc_train=torch.Tensor(self.num_epochs)
-    error_train=torch.Tensor(self.num_epochs)
-    start_time=time.clock()
-    for epoch in range(self.num_epochs):
-      print('----------------- the epoch {:d} -----------------' .format(epoch))
-      if (epoch == 1):
-        print("epoch = 1")
-      print('training epoch ',epoch)
-      for batch_idx, (data, target) in enumerate(train_loader):
-        batch_size=data.size(0)
-        input_64x784=data.view(batch_size,self.pixels)
-        target_64x10=torch.Tensor(batch_size,self.output_size)
-        for i in range(batch_size): target_64x10[i,:]=one_hot_encoding(target[i])
-        out=self.forward(input_64x784)
-        correct_batch=correct_batch+torch.sum(out.max(1)[1]==target)
-        #print('out max(1) is and target is', out.max(1), target)
-        #print(correct_batch)
+        ###TRAINING
+        time_meas = torch.Tensor(self.num_epochs)
+        acc_train = torch.Tensor(self.num_epochs)
+        error_train = torch.Tensor(self.num_epochs)
+        start_time = time.clock()
+        for epoch in range(self.num_epochs):
+            print('----------------- the epoch {:d} -----------------'.format(epoch))
+            if (epoch == 1):
+                print("epoch = 1")
+            print('training epoch ', epoch)
+            for batch_idx, (data, target) in enumerate(train_loader):
+                batch_size = data.size(0)
+                input_64x784 = data.view(batch_size, self.pixels)
+                target_64x10 = torch.Tensor(batch_size, self.output_size)
+                for i in range(batch_size): target_64x10[i, :] = one_hot_encoding(target[i])
+                out = self.forward(input_64x784)
+                correct_batch = correct_batch + torch.sum(out.max(1)[1] == target)
+                # print('out max(1) is and target is', out.max(1), target)
+                # print(correct_batch)
 
-        # where all bp happens here
-        loss=(self.model.backward(torch.t(target_64x10), eta=self.learning_rate).mean())
-        loss_train.append(loss)
+                # where all bp happens here
+                loss = (
+                    self.model.backward(torch.t(target_64x10), gd_bool=True).mean())  ## it replaces the updateParams
+                loss_train.append(loss)
+                # self.model.updateParams()
 
+                # print('debug')
+            correct_epoch.append(correct_batch)
+            print('train accuracy is {:.1f}% and loss is {:f}'.format(correct_epoch[-1].to(dtype=torch.float) / 600,
+                                                                      loss_train[-1]))
+            print('seconds elapsed is ', time.clock() - start_time)
+            time_meas[epoch] = time.clock() - start_time
+            acc_train[epoch] = correct_epoch[-1].to(dtype=torch.float) / 600
+            error_train[epoch] = loss_train[-1]
+            for batch_idx_test, (data_test, target_test) in enumerate(test_loader):
+                batch_size_test = data_test.size(0)
 
-        self.model.updateParams()
-        # print('debug')
-      correct_epoch.append(correct_batch)
-      print('train accuracy is {:.1f}% and loss is {:f}'.format(correct_epoch[-1].to(dtype=torch.float)/600, loss_train[-1]))
-      print('seconds elapsed is ', time.clock()- start_time)
-      time_meas[epoch] = time.clock()- start_time
-      acc_train[epoch] = correct_epoch[-1].to(dtype=torch.float)/600
-      error_train[epoch] = loss_train[-1]
-      for batch_idx_test, (data_test, target_test) in enumerate(test_loader):
-        batch_size_test = data_test.size(0)
+                # print(batch_size_test)
+                input_64x784 = data_test.view(batch_size_test, self.pixels)
+                target_64x10 = torch.Tensor(batch_size_test, self.output_size)
+                # print(target_64x10.shape)l
+                for i in range(batch_size_test): target_64x10[i, :] = one_hot_encoding(target_test[i])
+                out_test = self.forward(input_64x784)
+                # print(out_test.shape)
+                correct_batch_test = correct_batch_test + torch.sum(out_test.max(1)[1] == target_test)
+                # print(correct_batch_test)
+                loss_test.append(self.model.backward(torch.t(target_64x10)).mean())
+            correct_epoch_test.append(correct_batch_test)
+            print('For the epoch {:d}, test accuracy is {:.1f}% and loss is {:f}'.format(epoch,
+                                                                                         correct_epoch_test[-1].to(
+                                                                                             dtype=torch.float) / 100,
+                                                                                         loss_test[-1]))
+            correct_batch = 0  # for each epoch
+            correct_batch_test = 0
+        correct_epoch_percent = [correct_epoch[i] / 600 for i in range(len(correct_epoch))]
+        print('Here is your time to train', time_meas)
+        print('Here is your train error', error_train)
+        print('Here is your accuracy', acc_train)
 
-        # print(batch_size_test)
-        input_64x784 = data_test.view(batch_size_test, self.pixels)
-        target_64x10 = torch.Tensor(batch_size_test, self.output_size)
-        # print(target_64x10.shape)l
-        for i in range(batch_size_test): target_64x10[i, :] = one_hot_encoding(target_test[i])
-        out_test = self.forward(input_64x784)
-        # print(out_test.shape)
-        correct_batch_test = correct_batch_test + torch.sum(out_test.max(1)[1] == target_test)
-        # print(correct_batch_test)
-        loss_test.append(self.model.backward(torch.t(target_64x10)).mean())
-      correct_epoch_test.append(correct_batch_test)
-      print('For the epoch {:d}, test accuracy is {:.1f}% and loss is {:f}' .format(epoch, correct_epoch_test[-1].to(dtype=torch.float) / 100, loss_test[-1]))
-      correct_batch=0 # for each epoch
-      correct_batch_test = 0
-    correct_epoch_percent=[correct_epoch[i]/600 for i in range(len(correct_epoch))]
-    print('Here is your time to train',time_meas)
-    print('Here is your train error',error_train)
-    print('Here is your accuracy',acc_train)
+        ### TESTING
+        for batch_idx_test, (data_test, target_test) in enumerate(test_loader):
+            batch_size_test = data_test.size(0)
 
+            # print(batch_size_test)
+            input_64x784 = data_test.view(batch_size_test, self.pixels)
+            target_64x10 = torch.Tensor(batch_size_test, self.output_size)
+            # print(target_64x10.shape)
+            for i in range(batch_size_test): target_64x10[i, :] = one_hot_encoding(target_test[i])
+            out_test = self.forward(input_64x784)
+            # print(out_test.shape)
+            correct_batch_test = correct_batch_test + torch.sum(out_test.max(1)[1] == target_test)
+            # print(correct_batch_test)
+            loss_test.append(self.model.backward(torch.t(target_64x10)).mean())
+        correct_epoch_test.append(correct_batch_test)
+        print('test accuracy is {:.1f}% and loss is {:f}'.format(correct_epoch_test[-1].to(dtype=torch.float) / 100,
+                                                                 loss_test[-1]))
+        print('end')
 
-
-### TESTING
-    for batch_idx_test, (data_test, target_test) in enumerate(test_loader):
-      batch_size_test=data_test.size(0)
-
-      #print(batch_size_test)
-      input_64x784=data_test.view(batch_size_test,self.pixels)
-      target_64x10=torch.Tensor(batch_size_test,self.output_size)
-      #print(target_64x10.shape)
-      for i in range(batch_size_test): target_64x10[i,:]=one_hot_encoding(target_test[i])
-      out_test=self.forward(input_64x784)
-      #print(out_test.shape)
-      correct_batch_test=correct_batch_test+torch.sum(out_test.max(1)[1]==target_test)
-      #print(correct_batch_test)
-      loss_test.append(self.model.backward(torch.t(target_64x10)).mean())
-    correct_epoch_test.append(correct_batch_test)
-    print('test accuracy is {:.1f}% and loss is {:f}' .format(correct_epoch_test[-1].to(dtype=torch.float)/100, loss_test[-1]))
-    print('end')
-
-
-
-     
 # if __name__ == "__main__":
 #     main()
--- neural_network.py
+++ neural_network.py
@@ -1,189 +1,170 @@
-#import numpy as np TRY NOT TO USE THIS
+import numpy as np  # TRY NOT TO USE THIS
 import torch
 import math
 from collections import defaultdict
 
+from torch import Tensor
+
+
 class NeuralNetwork:
-  def  __init__(self, *argv):
-  # return a dict of theta matrices
-  # *argv will contain first the number of inputs, then any number of hidden layers, and then number of outputs
-    self.theta = defaultdict(list)
-    self.dE_dTheta = defaultdict(list)
-    self.activations=defaultdict(list)
-    self.weighted_sum=defaultdict(list)
-    self.num_outputs = argv[-1]
-    i=0
-    j=0
-    for arg in argv[0:-1]:
-      self.theta[i]=(torch.randn(arg+1,argv[i+1]))/math.sqrt(arg)
-      self.dE_dTheta[i]=torch.Tensor(arg+1,argv[i+1])
-      i+=1
-    for arg in argv[0:]:
-      self.activations[j]=torch.ones(arg+1)
-      self.weighted_sum[j]=torch.Tensor(arg)
-      j=j+1
-   # print('dict activations shape', self.activations)
-    
+    def __init__(self, *argv):
+        # return a dict of theta matrices
+        # *argv will contain first the number of inputs, then any number of hidden layers, and then number of outputs
+        self.theta = defaultdict(list)
+        self.dE_dTheta = defaultdict(list)
+        self.activations = defaultdict(list)
+        self.weighted_sum = defaultdict(list)
+        self.num_outputs = argv[-1]
+        self.layer = argv[0:-1]
+        print(self.layer)
+        i = 0
+        j = 0
+        for arg in argv[0:-1]:
+            self.theta[i] = (torch.randn(arg + 1, argv[i + 1])) / math.sqrt(arg)
+            self.dE_dTheta[i] = torch.Tensor(arg + 1, argv[i + 1])
+            i += 1
+        for arg in argv[0:]:
+            self.activations[j] = torch.ones(arg)
+            self.weighted_sum[j] = torch.Tensor(arg)
+            j = j + 1
 
-  # print('theta is',self.theta)
-  # print('de by theta is',self.dE_dTheta)
+    # print('dict activations shape', self.activations)
 
-  def getlayer(self,layer_num):
-    return self.theta[layer_num]
+    # print('theta is',self.theta)
+    # print('de by theta is',self.dE_dTheta)
 
-  def forward(self,inp):
-    
-    #in_flat=inp.view(inp.numel()) #flatten a 2D Input
-    #in_flat=[torch.Tensor.append(arg) for arg in argv]
-  #  print('input size is ',inp.size())
-    if len(inp.size())==1:
-      inp_exp=torch.ones(inp.numel(),1)
-      inp_exp[:,0]=inp
-      self.activations[0]=inp_exp
-      in_flat=torch.ones(inp.numel()+1,1)
-      in_flat[1:,0]=inp
-      output=torch.Tensor()
-    else:
-      self.activations[0]=inp
-      in_flat=torch.ones(inp.size(0)+1,inp.size(1))
-      in_flat[1:,:]=inp
-     # print('input to nn.forward are:',in_flat)
-      def sigmoid(arr): 
-        return  1 / (1 + torch.exp(-arr))
-    #assume this dimension and 
-   #   self.activations[0]=in_flat
-      self.weighted_sum[0]=0
-      for key, value in self.theta.items():
-        if key==0: 
-        #  print('key0 position',in_flat,value)
-          
-          z=torch.mm(torch.t(value),in_flat) #for input
-          output=sigmoid(z)
-          self.weighted_sum[key+1]=z
-          self.activations[key+1]=output
-          #   if key==[final case]=output[key]=sigmoid(torch.cmul(output[key-1],value)) #dont apply sigmoid to output? you have to apply for now
-        else: 
-        #  print('loop1')
-         # output_mod=torch.ones(output.numel()+1,1)
-          output_mod=torch.ones(output.size(0)+1,output.size(1))
-          output_mod[1:,:]=output
-          z=torch.mm(torch.t(value),output_mod)
-          output=sigmoid(z)
-          self.weighted_sum[key+1]=z
-          self.activations[key+1]=output
-    output_batch=output
-    return output_batch
-  # print('dict activations shape', self.activations)
-  # print('dict weighted sums shape', self.weighted_sum)
+    def getlayer(self, layer_num):
+        return self.theta[layer_num]
 
+    def forward(self, inp):
 
+        # in_flat=inp.view(inp.numel()) #flatten a 2D Input
+        # in_flat=[torch.Tensor.append(arg) for arg in argv]
+        #  print('input size is ',inp.size())
+        if len(inp.size()) == 1:
+            inp_exp = torch.ones(inp.numel(), 1)
+            inp_exp[:, 0] = inp
+            self.activations[0] = inp_exp
+            in_flat = torch.ones(inp.numel() + 1, 1)
+            in_flat[1:, 0] = inp
+            output = torch.Tensor()
+        else:
+            self.activations[0] = inp
+            in_flat = torch.ones(inp.size(0) + 1, inp.size(1))
+            in_flat[1:, :] = inp
 
- #   elif len(inp.size())>=1: #TODO: implement a and z for this too later, I'd say write this whole function again anyway
- #     self.activations[0]=inp
- #     output_batch=torch.Tensor(self.num_outputs,inp.size()[-1])
- #     #print('batch case')
- #     for b in range(inp.size()[-1]):
- #       in_flat=torch.ones(inp[:,b].numel()+1,1)
- #       in_flat[1:,0]=inp[:,b]
- #       output=torch.Tensor()
- #       #print('input to nn.forward are:',in_flat)
- #       def sigmoid(arr): 
- #         return  1 / (1 + torch.exp(-arr))
- #     #assume this dimension and 
- #       for key, value in self.theta.items():
- #         if key==0: 
- #           #print('key0 position',in_flat,value)
- #           output=sigmoid(torch.mm(torch.t(value),in_flat)) #for input
- #           self.activations[key+1][b,:,:]=output
- #           #   if key==[final case]=output[key]=sigmoid(torch.cmul(output[key-1],value)) #dont apply sigmoid to output? you have to apply for now
- #         else: 
- #           #print('loop1')
- #           output_mod=torch.ones(output.numel()+1,1)
- #           output_mod[1:,0]=output
- #           output=sigmoid(torch.mm(torch.t(value),output_mod))
- #       output_batch[:,b]=output
- #       self.activations[key+1][b,:,:]=output
-   # return output[-1]:reshape(input:size()) inc ase you want to return a 2D vector, but if it is a fully connected network, when will the output ever be 2d?
-   
-    return output_batch
-  
+            # print('input to nn.forward are:',in_flat)
+            def sigmoid(arr):
+                return 1 / (1 + torch.exp(-arr))
 
-  def backward(self,target,loss_criterion=None, eta):
-    if loss_criterion==None: loss_criterion='MSE'
-    numlayers=len(self.activations.keys())
-    def diff_sigmoid(matrix):  #to get dOUT/dNET
-      return matrix*(1-matrix)#only true for logistic, not for relu or other non-linearities
-    def cost_prime(output,target):
-      return output-target
-    if len(target.size())==1:
-      target_mid=torch.ones(target.size(0),1)
-      target_mid[:,0]=target
-      target=target_mid
-   # print('target is',target)
-   # print('output is',self.activations[numlayers-1])
-   # print('problem vec is',len(self.theta.keys()))
-    act=self.activations[numlayers-1]
-    diff=target-act
-    MSE_loss=torch.sum(torch.pow(diff,2),0)*0.5 
-    #print('MSE Loss is ', MSE_loss)
-    CE_Loss=torch.sum((-target*torch.log(act)-(1-target)*torch.log(1-act)))
-    #print('CE Loss is', CE_Loss)
-    if loss_criterion=='MSE':
-        delta=cost_prime(act,target)*diff_sigmoid(act)
-    elif loss_criterion=='CE':
-        delta=cost_prime(act,target)
- #  print('delta for last layer is or dE/dnet01', delta) #this corresponds to dE/dnet01
-    #print('de by theta is',self.dE_dTheta)
-    #problem is that dictionaries are not ordered
-    #print('problem shape',self.dE_dTheta[numlayers-2])
-    new_array=torch.Tensor(self.dE_dTheta[numlayers-2].size())
-  # print('prob vec is ',self.activations[numlayers-2].size())
-    new_array[0,:]=delta.mean(1) #for bias
-  # print('self.activations[numlayers-2],torch.t(delta) are',self.activations[numlayers-2],torch.t(delta))
-    new_array[1:,:]=torch.mm(self.activations[numlayers-2],torch.t(delta))/delta.size(1) #for weights
-    self.dE_dTheta[numlayers-2] = new_array
-    
-  # print('dE_dTheta for last layer is', self.dE_dTheta[numlayers-2])
-               
-    #backpropagate this into hidden layers:
-    numHiddenLayers=numlayers-2
-    for hid in range(numHiddenLayers):
-    # print('in hidden layer from back and from front =',hid,-hid-1)
-      relevant_weights=self.theta[numlayers-2-hid][1:,:]
-      relevant_activations=self.activations[numlayers-hid-2]
-      relevant_activations_prev=self.activations[numlayers-hid-3]
+            # assume this dimension and
+            #   self.activations[0]=in_flat
+            self.weighted_sum[0] = 0
+            for key, value in self.theta.items():
+                if key == 0:
+                    #  print('key0 position',in_flat,value)
 
-    # print('relevant_weights are',relevant_weights)
-    # print('relevant_activations are',relevant_activations)
-     # print('problem vec is ',relevant_weights.size(),delta.size(),relevant_activations.size())
-   # print('check sizes,',relevant_weights,delta,relevant_activations)
-      if loss_criterion=='MSE': delta=torch.mm(relevant_weights,delta)*diff_sigmoid(relevant_activations)
-      elif loss_criterion=='CE': delta=torch.mm(relevant_weights,delta)*diff_sigmoid(relevant_activations)
-    # print('delta for this hidden layer is or dE/dneth1', delta)  #this corresponds to dE/dneth1
-               
-      self.dE_dTheta[numlayers-3-hid][0,:]=delta.mean(1) #for bias
-    # print('problem vec is relevant_activations_prev,torch.t(delta)) ',relevant_activations_prev,torch.t(delta))
-      self.dE_dTheta[numlayers-3-hid][1:,:]=torch.mm(relevant_activations_prev,torch.t(delta))/delta.size(1) #for weights
-    # print('dE_dTheta for this layer is', self.dE_dTheta[numlayers-3-hid])
-               
-   #print('final delta is',self.dE_dTheta)
-    if loss_criterion=='MSE': return MSE_loss #TODO: remove later, just for testing
-    elif loss_criterion=='CE': return CE_Loss
-               
-               
-  def updateParams(self,):
-    # print('original theta is ', self.theta)
-    for key, value in self.theta.items():
-      #  if key==0:
-        #  print('key0 position',in_flat,value)
-      self.theta[key]=self.theta[key]-eta*self.dE_dTheta[key]
-      # print('thetakey0',self.theta[key])
-      # print('dthetakey0',self.dE_dTheta[key])
-      # print('updated Theta is', self.theta)
+                    z = torch.mm(torch.t(value), in_flat)  # for input
+                    output = sigmoid(z)
+                    self.weighted_sum[key + 1] = z
+                    self.activations[key + 1] = output
+                    #   if key==[final case]=output[key]=sigmoid(torch.cmul(output[key-1],value)) #dont apply sigmoid to output? you have to apply for now
+                else:
+                    #  print('loop1')
+                    # output_mod=torch.ones(output.numel()+1,1)
+                    output_mod = torch.ones(output.size(0) + 1, output.size(1))
+                    output_mod[1:, :] = output
+                    z = torch.mm(torch.t(value), output_mod)
+                    output = sigmoid(z)
+                    self.weighted_sum[key + 1] = z
+                    self.activations[key + 1] = output
+        output_batch = output
+        return output_batch
+        # print('dict activations shape', self.activations)
+        # print('dict weighted sums shape', self.weighted_sum)
 
-            
-               
-    
-               
-    
-    
+        return output_batch
+
+    def backward(self, target, loss_criterion=None, eta=0.1, gd_bool=False):
+        if loss_criterion == None: loss_criterion = 'MSE'
+        numlayers = len(self.activations.keys())
+
+        def diff_sigmoid(matrix):  # to get dOUT/dNET
+            return matrix * (1 - matrix)  # only true for logistic, not for relu or other non-linearities
+
+        def cost_prime(output, target):
+            return output - target
+
+        if len(target.size()) == 1:
+            target_mid = torch.ones(target.size(0), 1)
+            target_mid[:, 0] = target
+            target = target_mid
+        # print('target is',target)
+        # print('output is',self.activations[numlayers-1])
+        # print('problem vec is',len(self.theta.keys()))
+        act = self.activations[numlayers - 1]
+        diff = target - act
+        MSE_loss = torch.sum(torch.pow(diff, 2), 0) * 0.5
+        # print('MSE Loss is ', MSE_loss)
+        CE_Loss = torch.sum((-target * torch.log(act) - (1 - target) * torch.log(1 - act)))
+        # print('CE Loss is', CE_Loss)
+        if loss_criterion == 'MSE':
+            delta = cost_prime(act, target) * diff_sigmoid(act)
+        elif loss_criterion == 'CE':
+            delta = cost_prime(act, target)
+
+        new_array = torch.Tensor(self.dE_dTheta[numlayers - 2].size())  # type: Tensor
+        new_array[0, :] = delta.mean(1)  # for bias
+        new_array[1:, :] = torch.mm(self.activations[numlayers - 2], torch.t(delta)) / delta.size(1)  # for weights
+        self.dE_dTheta[numlayers - 2] = new_array
+
+        # print('dE_dTheta for last layer is', self.dE_dTheta[numlayers-2])
+
+        # backpropagate this into hidden layers:
+        numHiddenLayers = numlayers - 2
+        last_hidden_idx = len(self.layer) - 1
+        if gd_bool:
+            self.theta[0] = self.theta[0] - eta * self.dE_dTheta[0]
+
+        for hid in range(numHiddenLayers):
+            # print('in hidden layer from back and from front =',hid,-hid-1)
+            relevant_weights = self.theta[numlayers - 2 - hid][1:, :]
+            relevant_activations = self.activations[numlayers - hid - 2]
+            relevant_activations_prev = self.activations[numlayers - hid - 3]
+
+            # print('relevant_weights are',relevant_weights)
+            # print('relevant_activations are',relevant_activations)
+            # print('problem vec is ',relevant_weights.size(),delta.size(),relevant_activations.size())
+            # print('check sizes,',relevant_weights,delta,relevant_activations)
+            if loss_criterion == 'MSE':
+                delta = torch.mm(relevant_weights, delta) * diff_sigmoid(relevant_activations)
+            elif loss_criterion == 'CE':
+                delta = torch.mm(relevant_weights, delta) * diff_sigmoid(relevant_activations)
+            # print('delta for this hidden layer is or dE/dneth1', delta)  #this corresponds to dE/dneth1
+
+            self.dE_dTheta[numlayers - 3 - hid][0, :] = delta.mean(1)  # for bias
+            # print('problem vec is relevant_activations_prev,torch.t(delta)) ',relevant_activations_prev,torch.t(delta))
+            self.dE_dTheta[numlayers - 3 - hid][1:, :] = torch.mm(relevant_activations_prev,
+                                                                  torch.t(delta)) / delta.size(1)  # for weights
+            # print('dE_dTheta for this layer is', self.dE_dTheta[numlayers-3-hid])
+            if gd_bool:
+                self.theta[numlayers - 2 - hid] = self.theta[numlayers - 2 - hid] - eta * self.dE_dTheta[
+                    numlayers - 2 - hid]
+
+        # self.theta[0] = self.theta[0] - 1 * self.dE_dTheta[0]
+        # print('final delta is',self.dE_dTheta)
+        if loss_criterion == 'MSE':
+            return MSE_loss  # TODO: remove later, just for testing
+        elif loss_criterion == 'CE':
+            return CE_Loss
+
+    # # this could not be bypassed, the list limitation just like numpy
+    # def updateParams(self, eta):
+    #     # print('original theta is ', self.theta)
+    #     for key, value in self.theta.items():
+    #         #  if key==0:
+    #         #  print('key0 position',in_flat,value)
+    #         self.theta[key] = self.theta[key] - eta * self.dE_dTheta[key]
+    #         # print('thetakey0',self.theta[key])
+    #         # print('dthetakey0',self.dE_dTheta[key])
+    #         # print('updated Theta is', self.theta)
