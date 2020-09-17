1e7c9d67b0e7828024e792a469060bdfee5c084f
HaFred <zhongad@connect.ust.hk>
2020-09-17 18:34:47 +0800

    update

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from my_img2num import MyImg2Num
 from nn_img2num import NnImg2Num
 
 def main ():
-  # model_my = MyImg2Num()
-  model_my = NnImg2Num()
+  model_my = MyImg2Num()
+  # model_my = NnImg2Num()
   model_my.train()
   print('done')
 
